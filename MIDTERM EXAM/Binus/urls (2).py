37ef69c410ebc8d9772a66dd9bc94957dbcac930
Abdur-Rawf <rawf.ict20@gmail.com>
2025-10-29 00:30:05 +0600

    Delete MIDTERM EXAM/Binus directory

--- MIDTERM EXAM/Binus/urls (2).py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('students/', views.student_list, name='student_list'),
-    path('students/add/', views.add_student, name='add_student'),
-    path('students/delete/<int:id>/', views.delete_student, name='delete_student'),
-]
